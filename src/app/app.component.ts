c7dc8be5a096a388c7605b4a81dfa0bcf9fbc50b
Maicon Lanzendorf <maicon.lanzendorf@live.com>
2021-10-03 14:30:28 -0300

    adicionado versionamento da aplicação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {PrimeNGConfig} from 'primeng/api';
+import {environment} from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -8,11 +9,17 @@ import {PrimeNGConfig} from 'primeng/api';
 })
 export class AppComponent implements OnInit {
   title = 'championship-frontend';
+  appVersion: string;
+  color: string;
 
   constructor(private primengConfig: PrimeNGConfig) {
   }
 
   ngOnInit(): void {
+    this.appVersion = '1.5.0';
+    this.color = 'blue';
+
+    this.appVersion = `https://img.shields.io/badge/Vers%C3%A3o-${this.appVersion}-${this.color}` ;
     this.primengConfig.ripple = true;
   }
 }
