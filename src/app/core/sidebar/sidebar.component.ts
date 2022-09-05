55eaa4da9d4b0da8a03db6fb236748f63b130862
Maicon Lanzendorf <maicon.lanzendorf@live.com>
2022-09-04 22:57:28 -0300

    update version

--- src/app/core/sidebar/sidebar.component.ts
+++ src/app/core/sidebar/sidebar.component.ts
@@ -25,7 +25,7 @@ export class SidebarComponent implements OnInit {
               private auth: AuthService) { }
 
   ngOnInit(): void {
-    this.appVersion = '1.5.5';
+    this.appVersion = '1.5.6';
     this.color = 'blue';
     this.apiColorcolor = 'navy';
 
